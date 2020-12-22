eab99f36b2373ae9cda9f8f7bc24d631dd394b86
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-12-23 00:52:43 +0100

    pod install + display views and routes setup

---
+++ pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+class Home extends React.Component {
+  render(){
+    return (
+      <ScrollView>
+      <Text> Home </Text>
+      </ScrollView>
+    )
+  }
+}
+
+export default Home
---
+++ pages/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ScrollView} from 'react-native';
+
+class Search extends React.Component {
+  render(){
+    return (
+      <ScrollView>
+      </ScrollView>
+    )
+  }
+}
+
+export default Search
